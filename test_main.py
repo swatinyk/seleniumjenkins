565325044434a3863553db021e6338ce554a38f4
swatinyk <59159628+swatinyk@users.noreply.github.com>
2023-01-10 15:24:22 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -13,7 +13,7 @@ def test_login():
     options = Options()
     # options.binary_location = "/usr/local/bin/chromedriver"    #chrome binary location specified here
     options.add_argument("--no-sandbox")
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--start-maximized") #open Browser in maximized mode
     driver=webdriver.Chrome(options=options,executable_path=r'/usr/local/bin/chromedriver')
     # driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
